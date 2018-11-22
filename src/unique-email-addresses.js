724abb0379d4584eedceb7f97285f4bc3d4847e3
cobish <cobish110@gmail.com>
2018-11-22 09:42:35 +0800

    独特的电子邮件地址

---
+++ src/unique-email-addresses.js
@@ -0,0 +1,35 @@
+/**
+ * 独特的电子邮件地址
+ * 
+ * https://leetcode-cn.com/problems/unique-email-addresses/description/
+ */
+
+/**
+ * @param {string[]} emails
+ * @return {number}
+ */
+var numUniqueEmails = function(emails) {
+  var hashMap = {};
+  var num = 0;
+
+  for (var i = 0; i < emails.length; i++) {
+    var email = emails[i];
+
+    var emailArr = email.split('@');
+    var local = emailArr[0];
+    var domain = emailArr[1];
+
+    local = local.split('+')[0];
+    local = local.replace(/\./g, '');
+
+    var newEmail = local + '@' + domain;
+    if (!hashMap[newEmail]) {
+      num += 1;
+      hashMap[newEmail] = newEmail;
+    }
+  }
+
+  return num;
+};
+
+module.exports = numUniqueEmails;
---
+++ test/unique-email-addresses.test.js
@@ -0,0 +1,11 @@
+const numUniqueEmails = require('../src/unique-email-addresses.js');
+
+test('示例 1-1', () => {
+  const arr = [
+    'test.email+alex@leetcode.com',
+    'test.e.mail+bob.cathy@leetcode.com',
+    'testemail+david@lee.tcode.com'
+  ];
+
+  expect(numUniqueEmails(arr)).toBe(2);
+});
